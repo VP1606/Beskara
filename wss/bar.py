8c013674f648be1dd3b31e57dd0b8c50c0503423
VP1606 @ 16MBP <vpremakantha@gmail.com>
2023-07-16 16:35:27 +0100

    Added cores of bartender from viperblade.

--- wss/bar.py
+++ wss/bar.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, WebSocket
+import bartender as BT
 
 app = FastAPI()
+manager = BT.BarTender()
 
 @app.websocket("/wss")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+    await manager.connect(websocket)
     while True:
         data = await websocket.receive_text()
         print(data)
---
+++ wss/bartender.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from fastapi import WebSocket
+import uuid
+import json
+
+@dataclass
+class BarTender:
+    def __init__(self) -> None:
+        self.active_connections: dict = {}
+        self.uuid_to_userid: dict = {}
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        new_uuid = str(uuid.uuid4())
+
+    def disconnect(self, websocket: WebSocket):
+        loc_uuid = self.find_connection_id(websocket)
+
+    def find_connection_id(self, websocket: WebSocket):
+        locked_list = self.active_connections
+
+        val_list = list(locked_list.values())
+        key_list = list(locked_list.keys())
+
+        loc_uuid = val_list.index(websocket)
+        return key_list[loc_uuid]
+    
+    async def targeted_send_package(self, websocket: WebSocket, payload: json):
+        await websocket.send_json(payload)
+
+    async def broadcast_package(self, payload: json):
+        for connection in self.active_connections.values():
+            await connection.send_json(payload)
