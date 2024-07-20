0e10fc81b397f45dc0083ebf9e1149da7a71e1ab
VP1606 @ 16MBP <vpremakantha@gmail.com>
2024-07-20 21:05:28 +0100

    Added new working metal code.

--- metal/operations.py
+++ metal/operations.py
@@ -1,14 +1,18 @@
-from gpiozero import OutputDevice, InputDevice
+from gpiozero import OutputDevice, InputDevice ## Deprecate!
+import RPi.GPIO as GPIO
+
+def setup():
+    print("Setup GPIO ports")
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(16, GPIO.OUT)
 
 def open_valve(gpio_port):
-    valve = OutputDevice(gpio_port)
-    valve.on()
-    print(f"Opening valve on GPIO port {gpio_port}")
+    GPIO.output(16, GPIO.HIGH)
+    print(f"Opened valve on GPIO port {gpio_port}")
 
 def close_valve(gpio_port):
-    valve = OutputDevice(gpio_port)
-    valve.off()
-    print(f"Closing valve on GPIO port {gpio_port}")
+    GPIO.output(16, GPIO.LOW)
+    print(f"Closed valve on GPIO port {gpio_port}")
 
 def read_flm(gpio_port):
     flow_meter = InputDevice(gpio_port)
@@ -16,7 +20,6 @@ def read_flm(gpio_port):
 
     print(f"Reading flow meter on GPIO port {gpio_port}")
     return flow_value
-
 
 # Hello this is angellou, I am gonna do DEZ commands on the UBUNTU server  
 #sudo apt update
